fe0a04aa15489d612e82b41321e52debf78109d6
Duck-sot <raja07001@utb.vaxjo.se>
2024-08-28 11:18:13 +0200

    Första C# project
    detta har en  miniräknare samt ett program som sätter ihop dit för namn och efter namn

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Could you give me tow numbers");
+string text = Console.ReadLine();
+string text2 = Console.ReadLine();
+int tal1 =int.Parse(text);
+int tal2 =int.Parse(text2);
+Console.WriteLine(tal1 + tal2);
+Console.WriteLine(tal1 - tal2); 
+
+ /*
+ Console.WriteLine("what is your first name");
+ string text = Console.ReadLine();
+ Console.WriteLine("what is your sirname");
+ String text2 = Console.ReadLine();
+ Console.WriteLine(text +" "+ text2);
+ */
